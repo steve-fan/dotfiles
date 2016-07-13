b05efba43885b87f9db5b3e32003f45af324de62
steve-fan <fanzc.daily@gmail.com>
2016-07-13 12:15:18 +0800

    copy text from emacs to osx clipboard

--- emacs/.emacs.d/core/core-custom.el
+++ emacs/.emacs.d/core/core-custom.el
@@ -39,4 +39,12 @@
    )
  )
 
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+
 (provide 'core-custom)
